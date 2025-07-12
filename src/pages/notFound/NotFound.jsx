55e6ff9873970e9254e61273eff3561fadbb8343
Alisher Murodov <alisher120102@gmail.com>
2025-07-12 23:46:07 +0500

    fix

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -4,6 +4,7 @@ import { Button } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 const NotFound = () => {
+  console.log();
   return (
     <>
       <div className={styles.container}>
